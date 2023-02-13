78b4196f4c9ff8f1e0918fb134a82bc0c98ffd67
PCI173 <PCI173@PCI173>
2023-02-13 12:24:04 +0530

    Add project files.

---
+++ CI Platform .net/Controllers/UserPages.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CI_Platform_.net.Controllers
+{
+    public class UserPages : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
